815e10bb1c4dbcbb3851bfcf1248bc8486b9b052
gagpoghosyan99 <gagpoghosyandt@gmail.com>
2026-03-06 09:53:25 +0100

    fix(settings): ignore extra env vars (pydantic) + stabilize accounts serializer

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
 
+        extra = "ignore"  # allow unknown env vars
 
 # Global settings instance
 settings = Settings()
